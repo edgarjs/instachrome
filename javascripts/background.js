99c138c0b1678c3a32a75f7e38c832c3091c3299
Gordon Fontenot <gordon.fontenot@gmail.com>
2010-07-20 05:55:49 +0800

    Added ability to save base URL as article description

--- javascripts/background.js
+++ javascripts/background.js
@@ -4,37 +4,71 @@ var badge_styles = {
     ICON: 1,
     CHROMED: 2
 };
+var description_styles = {
+    DEFAULT: 0,
+    SITEURL: 1
+};
 
 function onComplete(xhr) {
-    if(xhr.srcElement.status == 201) {
-        if($.db('auto_close') === '1') {
+    if (xhr.srcElement.status == 201) {
+        if ($.db('auto_close') === '1') {
             chrome.tabs.remove(last_tab_id);
         } else {
-            switch(parseInt($.db('badge_style'))) {
-                case badge_styles.TEXT:
-                    chrome.browserAction.setBadgeBackgroundColor({color: [53, 181, 49, 255], tabId: last_tab_id});
-                    chrome.browserAction.setBadgeText({text: 'ok', tabId: last_tab_id});
-                    break;
-                case badge_styles.ICON:
-                    chrome.browserAction.setIcon({path: 'images/saved.png', tabId: last_tab_id});
-                    break;
-                case badge_styles.CHROMED:
-                    chrome.browserAction.setIcon({path: 'images/chromed_saved.png', tabId: last_tab_id});
-                    break;
+            switch (parseInt($.db('badge_style'))) {
+            case badge_styles.TEXT:
+                chrome.browserAction.setBadgeBackgroundColor({
+                    color:
+                    [53, 181, 49, 255],
+                    tabId: last_tab_id
+                });
+                chrome.browserAction.setBadgeText({
+                    text: 'ok',
+                    tabId: last_tab_id
+                });
+                break;
+            case badge_styles.ICON:
+                chrome.browserAction.setIcon({
+                    path:
+                    'images/saved.png',
+                    tabId: last_tab_id
+                });
+                break;
+            case badge_styles.CHROMED:
+                chrome.browserAction.setIcon({
+                    path:
+                    'images/chromed_saved.png',
+                    tabId: last_tab_id
+                });
+                break;
             }
         }
     } else {
-        switch(parseInt($.db('badge_style'))) {
-            case badge_styles.TEXT:
-            chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255], tabId: last_tab_id});
-                chrome.browserAction.setBadgeText({text: 'err!', tabId: last_tab_id});
-                break;
-            case badge_styles.ICON:
-                chrome.browserAction.setIcon({path: 'images/error.png', tabId: last_tab_id});
-                break;
-            case badge_styles.CHROMED:
-                chrome.browserAction.setIcon({path: 'images/chromed_error.png', tabId: last_tab_id});
-                break;
+        switch (parseInt($.db('badge_style'))) {
+        case badge_styles.TEXT:
+            chrome.browserAction.setBadgeBackgroundColor({
+                color:
+                [255, 0, 0, 255],
+                tabId: last_tab_id
+            });
+            chrome.browserAction.setBadgeText({
+                text: 'err!',
+                tabId: last_tab_id
+            });
+            break;
+        case badge_styles.ICON:
+            chrome.browserAction.setIcon({
+                path:
+                'images/error.png',
+                tabId: last_tab_id
+            });
+            break;
+        case badge_styles.CHROMED:
+            chrome.browserAction.setIcon({
+                path:
+                'images/chromed_error.png',
+                tabId: last_tab_id
+            });
+            break;
         }
     }
 }
@@ -46,72 +80,101 @@ function sendRequest(url, selection) {
     var username = $.db('username');
     var password = $.db('password') || '';
 
-    if(!username) {
+    if (!username) {
         var tabImg = 'images/default.png';
-        if(parseInt($.db('badge_style')) === badge_styles.CHROMED) {
+        if (parseInt($.db('badge_style')) === badge_styles.CHROMED) {
             tabImg = 'images/chromed_default.png';
         }
-        chrome.browserAction.setIcon({path: tabImg, tabId: last_tab_id});
-        chrome.tabs.create({url: chrome.extension.getURL('options.html')});
+        chrome.browserAction.setIcon({
+            path: tabImg,
+            tabId: last_tab_id
+        });
+        chrome.tabs.create({
+            url: chrome.extension.getURL('options.html')
+        });
         return;
     }
-    
+
     xhr.onreadystatechange = onComplete;
     chrome.tabs.get(last_tab_id, function(tab) {
+
+        var theDescript = "Saved from Instachrome";
+        if (parseInt($.db('description_style')) === description_styles.SITEURL) {
+            theDescript = tab.url.match(/https?:\/\/([^\/]+)/)[1]
+        }
+
         var title = encodeURIComponent(tab.title);
         var params = {
             url: url,
             username: username,
             password: password,
-            selection: selection || "Saved from instachrome extension"
+            selection: selection || theDescript
         };
         var title = title ? ("&title=" + title) : "&auto-title=1";
         xhr.open("GET", 'https://www.instapaper.com/api/add?' +
-            $.param(params) + title, true);
+        $.param(params) + title, true);
         xhr.send();
     });
 }
 
 function readLater(tab, selection) {
     last_tab_id = tab.id;
-    switch(parseInt($.db('badge_style'))) {
-        case badge_styles.TEXT:
-            // TODO Customize color
-            chrome.browserAction.setBadgeBackgroundColor({color: [82, 168, 207, 255], tabId: last_tab_id});
-            chrome.browserAction.setBadgeText({text: '...', tabId: last_tab_id});
-            break;
-        case badge_styles.ICON:
-            chrome.browserAction.setIcon({path: 'images/saving.png', tabId: last_tab_id});
-            break;
-        case badge_styles.CHROMED:
-            chrome.browserAction.setIcon({path: 'images/chromed_saving.png', tabId: last_tab_id});
-            break;
+    switch (parseInt($.db('badge_style'))) {
+    case badge_styles.TEXT:
+        // TODO Customize color
+        chrome.browserAction.setBadgeBackgroundColor({
+            color:
+            [82, 168, 207, 255],
+            tabId: last_tab_id
+        });
+        chrome.browserAction.setBadgeText({
+            text: '...',
+            tabId: last_tab_id
+        });
+        break;
+    case badge_styles.ICON:
+        chrome.browserAction.setIcon({
+            path:
+            'images/saving.png',
+            tabId: last_tab_id
+        });
+        break;
+    case badge_styles.CHROMED:
+        chrome.browserAction.setIcon({
+            path:
+            'images/chromed_saving.png',
+            tabId: last_tab_id
+        });
+        break;
     }
     sendRequest(tab.url, selection);
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId) {
     var tabImg = 'images/default.png';
-    if(parseInt($.db('badge_style')) === badge_styles.CHROMED) {
+    if (parseInt($.db('badge_style')) === badge_styles.CHROMED) {
         tabImg = 'images/chromed_default.png';
     }
-    chrome.browserAction.setIcon({path: tabImg, tabId: tabId});
+    chrome.browserAction.setIcon({
+        path: tabImg,
+        tabId: tabId
+    });
 });
 chrome.browserAction.onClicked.addListener(readLater);
 // from key shortcut
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if(sender.tab) {
-        if(request.hasOwnProperty('keystroke')) {
+    if (sender.tab) {
+        if (request.hasOwnProperty('keystroke')) {
             var shortcut = $.db('shortcut');
             var ctrlKey = (shortcut.ctrlKey == request.keystroke.ctrlKey),
-                altKey = (shortcut.altKey == request.keystroke.altKey),
-                shiftKey = (shortcut.shiftKey == request.keystroke.shiftKey),
-                keyCode = (shortcut.keyCode == request.keystroke.keyCode);
+            altKey = (shortcut.altKey == request.keystroke.altKey),
+            shiftKey = (shortcut.shiftKey == request.keystroke.shiftKey),
+            keyCode = (shortcut.keyCode == request.keystroke.keyCode);
             sendResponse({
                 shortcutPressed: (ctrlKey && altKey && shiftKey & keyCode)
             });
-        } else if(request.hasOwnProperty('selection')) {
+        } else if (request.hasOwnProperty('selection')) {
             readLater(sender.tab, request.selection);
         }
     }
- });
+});
--- javascripts/options.js
+++ javascripts/options.js
@@ -16,63 +16,67 @@ var Options = function() {
             altKey: true,
             ctrlKey: false,
             shiftKey: true
-        }
+        },
+        description_style: 0
     };
-    
+
     var ui = {
         username: $('input#username'),
         password: $('input#password'),
         show_popup: $('input#show_popup'),
         auto_close: $('input#auto_close'),
         badge_style: $('select#badge_style'),
-        shortcut: $('input#shortcut')
+        shortcut: $('input#shortcut'),
+        description_style: $('select#description_style')
     };
-    
+
     var dbOrDefault = function(db_key) {
         return $.db(db_key) || values[db_key];
     };
-    
+
     return {
         humanizeKeystrokes: function(e) {
             var key = String.fromCharCode(e.keyCode);
             var special = "";
-            if(e.ctrlKey) {
+            if (e.ctrlKey) {
                 special += "Ctrl + ";
             }
-            if(e.altKey) {
+            if (e.altKey) {
                 special += "Alt + ";
             }
-            if(e.shiftKey) {
+            if (e.shiftKey) {
                 special += "Shift + ";
             }
             return special + key;
         },
-        
+
         restore: function() {
-            if(location.hash === '#setup') {
+            if (location.hash === '#setup') {
                 $.flash('Enter your Instapaper credentials in order to save an URL.');
             }
             ui.username.val(dbOrDefault('username'));
             ui.password.val(dbOrDefault('password'));
-            if(dbOrDefault('show_popup') === '1') {
+            if (dbOrDefault('show_popup') === '1') {
                 ui.show_popup.attr('checked', true);
             }
-            if(dbOrDefault('auto_close') === '1') {
+            if (dbOrDefault('auto_close') === '1') {
                 ui.auto_close.attr('checked', true);
             }
             ui.badge_style.val(dbOrDefault('badge_style'));
             ui.shortcut.val(this.humanizeKeystrokes(dbOrDefault('shortcut')))
-                .data('keys', dbOrDefault('shortcut'));
+            .data('keys', dbOrDefault('shortcut'));
+            ui.description_style.val(dbOrDefault('description_style'));
         },
 
         save: function() {
             $.db('username', ui.username.val());
             $.db('password', ui.password.val());
-            $.db('show_popup', ui.show_popup.is(':checked') ? '1' : '0');
-            $.db('auto_close', ui.auto_close.is(':checked') ? '1' : '0');
+            $.db('show_popup', ui.show_popup.is(':checked') ? '1': '0');
+            $.db('auto_close', ui.auto_close.is(':checked') ? '1': '0');
             $.db('badge_style', ui.badge_style.val());
             $.db('shortcut', ui.shortcut.data('keys'));
-            
+            $.db('description_style', ui.description_style.val());
+
             $.flash('Options saved successfully!');
         }
     };
@@ -82,21 +86,21 @@ var Options = function() {
 $(function() {
     var o = new Options();
     o.restore();
-    
+
     $('.flash').click(function() {
         $(this).fadeOut('slow');
         return false;
     });
-    
+
     $('.close').click(function() {
         window.close();
     });
-    
+
     $('form.options').submit(function() {
         o.save();
         return false;
     });
-    
+
     $('#shortcut').keydown(function(e) {
         $(this).val(o.humanizeKeystrokes(e)).data('keys', {
             ctrlKey: e.ctrlKey,
